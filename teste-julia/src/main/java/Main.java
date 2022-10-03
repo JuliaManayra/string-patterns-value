28be6628d556b7717a51269466c479d3ef9d46ac
JuliaManayra <35712868+JuliaManayra@users.noreply.github.com>
2022-10-02 21:27:12 -0300

    Update Main.java

--- teste-julia/src/main/java/Main.java
+++ teste-julia/src/main/java/Main.java
@@ -13,7 +13,7 @@ public class Main {
         System.out.println("entrada: b(a){4}juli(o){4} ->  SAIDA:  "+ pattern.getValuePattern("b(a){4}juli(o){4}"));
         System.out.println("entrada: b(o){4}juli(a){4}test(e){2} ->  SAIDA:  "+ pattern.getValuePattern("b(o){4}juli(a){4}test(e){2}"));
         System.out.println("entrada: b(o){10}juli(a){2}test(e){2} ->  SAIDA:  "+ pattern.getValuePattern("b(o){10}juli(a){2}test(e){2}"));
-        System.out.println("entrada: l(u){2}i(z){2}ca(r){2}l(o){2}(s){3} ->  SAIDA:  "+ pattern.getValuePattern("l(u){2}i(z){2}ca(r){2}l(o){2}(s){3}"));
+        System.out.println("entrada: j(u){2}li(a){2}man(a){2}(y){2}r(a){3} ->  SAIDA:  "+ pattern.getValuePattern("j(u){2}li(a){2}man(a){2}(y){2}r(a){3}"));
         System.out.println("entrada: a(bo){2}ra ->  SAIDA:  "+ pattern.getValuePattern("a(bo){2}ra"));
         System.out.println("entrada: a(bo(ki){2}){2}ra ->  SAIDA:  "+ pattern.getValuePattern("a(bo(ki){2}){2}ra"));
         System.out.println("entrada: b(a){4} ->  SAIDA:  "+ pattern.getValuePattern("b(a){4}"));
